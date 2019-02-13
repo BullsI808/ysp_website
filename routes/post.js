aafa9b5ce93fa084e68bcfcb5fd2d414b7946ba1
BullsI808 <JBasuil8697@myhta.org>
2019-02-13 11:33:29 -1000

    added post code, just need to change some stuff around for the new database

--- routes/post.js
+++ routes/post.js
@@ -1,1 +1,58 @@
+// justin work on post..... copy over code from movie list and change what is needed
+'use strict';
+const Pool = require('pg-pool');
+const config = require('../config.json');
+const {table, host, database, user, password, port} = config;
+const pool = new Pool({
+  host,
+  database,
+  user,
+  password,
+  port,
+  idleTimeoutMillis: 1000
+});
+
+
+//change name to make it fit
+module.exports.postMovie = (event, context, callback) => {
+console.log('event', event);
+
+//change vars to make it work
+
+let name = event.body.movie_title;
+let releaseDate = event.body.movie_release_date;
+let genre = event.body.movie_genre;
+
+ const insertNewMovie = `INSERT INTO ${table} VALUES(default, $1 , $2 , $3)`;
+
+ pool.connect()
+  .then(client =>{
+    client.release()
+    return client.query(insertNewMovie, [name, releaseDate, genre])
+  })
+  .then(res =>{
+  
+ 
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': 'http://127.0.0.1:8080',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      message: res,
+      input: event,
+    }),
+  };
+
+  callback(null , response)
+})
+
+.catch(err =>{
+  console.log('err', err);
+})
+  
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
 

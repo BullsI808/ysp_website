961ee873da71b30b2132ea756948fed34079daef
BullsI808 <JBasuil8697@myhta.org>
2019-02-26 14:27:40 -1000

    added base get code

--- public/index.js
+++ public/index.js
@@ -2,6 +2,19 @@ console.log("hello there");
 
 var btn1 = document.querySelector('#postTest');
 
+
+//get 
+
+axios
+    .get('https://j4m4803yml.execute-api.us-west-2.amazonaws.com/dev/getContent')
+    .then(
+        function(response){
+            console.log(response);
+        });
+
+
+
+//post
 btn1.addEventListener('click', ()=>{
     let data = {
         movie_title: postTitle.value,
@@ -10,7 +23,7 @@ btn1.addEventListener('click', ()=>{
     };
     console.log('typeof postReleaseDate.value', typeof postReleaseDate.value);
     axios
-     .post ('https://j4m4803yml.execute-api.us-west-2.amazonaws.com/dev/postMovie',data)
+     .post ('https://j4m4803yml.execute-api.us-west-2.amazonaws.com/dev/postContent',data)
      .then(data=>{
          console.log('data', data);
      })

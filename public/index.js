223bf25a2499a1b018b338aa3b65d7fad4dcf722
BullsI808 <JBasuil8697@myhta.org>
2019-02-26 15:00:26 -1000

    added get and post. get is working but post is not

--- public/index.js
+++ public/index.js
@@ -1,30 +1,35 @@
 console.log("hello there");
 
-var btn1 = document.querySelector('#postTest');
-
+var input1= document.querySelector('#testContent');
+var btn1 = document.getElementById('postTest');
+console.log(btn1);
 
 //get 
 
-axios
-    .get('https://j4m4803yml.execute-api.us-west-2.amazonaws.com/dev/getContent')
+
+axios.get('https://j4m4803yml.execute-api.us-west-2.amazonaws.com/dev/getContent')
     .then(
         function(response){
             console.log(response);
+        })
+        .catch(function(err){
+            console.log('err', err);
         });
 
 
 
 //post
-btn1.addEventListener('click', ()=>{
-    let data = {
-        movie_title: postTitle.value,
-        movie_genre: postGenre.value,
-        movie_release_date: Number(postReleaseDate.value)
-    };
-    console.log('typeof postReleaseDate.value', typeof postReleaseDate.value);
-    axios
-     .post ('https://j4m4803yml.execute-api.us-west-2.amazonaws.com/dev/postContent',data)
-     .then(data=>{
-         console.log('data', data);
-     })
-})
+
+// btn1.addEventListener('click', ()=>{
+//     let data = {
+//         item_data: input1.value
+//     };
+//     axios
+//      .post ('https://j4m4803yml.execute-api.us-west-2.amazonaws.com/dev/postContent',data)
+//      .then(data=>{
+//          console.log('data', data);
+//      })
+// })
+// .catch(function(err){
+//     console.log('err', err);
+// });

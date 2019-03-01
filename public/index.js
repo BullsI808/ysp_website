0b31a67f1bd12439440ce9f6df876b3972cd3c01
BullsI808 <JBasuil8697@myhta.org>
2019-02-28 14:57:34 -1000

    post is now working

--- public/index.js
+++ public/index.js
@@ -21,12 +21,14 @@ axios.get('https://j4m4803yml.execute-api.us-west-2.amazonaws.com/dev/getContent
 //post
 
  btn1.addEventListener('click', ()=>{
+     console.log(input1.value);
      let data = {
          item_data: input1.value
      };
       axios.post ('https://j4m4803yml.execute-api.us-west-2.amazonaws.com/dev/postContent',data)
       .then(data=>{
           console.log('data', data);
+        
       })
  })
 // .catch(function(err){
